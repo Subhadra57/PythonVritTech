c25ba0f63d7e929b43e1a68a24bcf40c72bf2861
unknown <sapkotasubhadra30@gmail.com>
2024-08-09 19:49:28 +0545

    assign

---
+++ 13_assignment.py
@@ -0,0 +1,14 @@
+#wap to make a calculator.
+
+#algorithm of calculator
+#first-step :ask first number from user
+#second-step: ask operator from user
+#third-step: ask second number from users
+#fourth-step: perform operation based on operator
+
+if operator =='+'
+    add
+elif operator =='-':
+    perform operation.
+
+    #wap to find input number is prime or composite 
