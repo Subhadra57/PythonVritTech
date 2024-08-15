fa73b3e5825763501e3031f7e88e79e28156ccbb
unknown <sapkotasubhadra30@gmail.com>
2024-08-15 18:39:15 +0545

    list ongoing

--- 26_list.py
+++ 26_list.py
@@ -1,4 +1,66 @@
-name = "january"
+# # name = "month"
 
-name_list = ["januray","march","may"]
-print(name_list)
+# # name_list = ["januray","march","may"]
+# # print(name_list)
+
+# #list constructor
+
+# # name_list = ["ram", "hari",1,2,3]
+
+# name_list = ["ram","hari",1,5,9]
+
+# # print(name_list[0])
+
+# # print(name_list[-2])
+
+# # print(name_list[start:end:step])
+
+# # print(name_list[5::-2])
+
+# #add element in list
+# name_list.append("kathmandu")
+# name_list.append("biratnagar")
+
+
+# #insert item in list
+# name_list.insert(2,"bhaktapur")
+
+
+# #pop item from list by positioin
+# name_list.pop(0)
+
+
+# #remove item from list
+# name_list.remove("hari")
+
+
+# #find length of list
+# print(len(name_list))
+
+# #replace item from element
+# name_list[0] = "india"
+# print(name_list)
+
+
+# #Accessing item using loop
+
+
+
+
+#class work find highes digit from list [1,5,9,6,89,1203,4,23]
+list_number = [1,5,9,6,89,1203,4,23]
+print (max(list_number))
+
+#class work to short given list [1,5,9,6,89,1203,4,23]
+list_number.sort()
+print(list_number)
+
+#swap number a=50,b=80
+a = 50
+b = 80
+print("before swap a,b=",a,":"b)
+temp = a
+a = b
+b = temp
+print("before swap a,b=",a,":"b)
+
